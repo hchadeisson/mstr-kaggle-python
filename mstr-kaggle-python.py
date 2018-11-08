926b2c0f087903e7c8c1c6242419d50f950ab0a2
HF <henrifrancois.chadeisson@gmail.com>
2018-11-08 23:08:29 +0100

    Added verbose execution

--- mstr-kaggle-python.py
+++ mstr-kaggle-python.py
@@ -3,23 +3,28 @@
 # 2. Enable internet (Kaggle beta feature)
 
 # mstr initialization
-username = 'my_username' #changeme
-password = 'my_password' #changeme
-base_url = 'https://yourserver.com/MicroStrategyLibrary/api' #changeme
-project_name = 'MicroStrategy Tutorial'
+mstr_username = 'my_username' #changeme
+mstr_password = 'my_password' #changeme
+mstr_library_api_url = 'https://yourserver.com/MicroStrategyLibrary/api' #changeme
+mstr_project_name = 'MicroStrategy Tutorial'
 
 import os
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from mstrio import microstrategy # MicroStrategy library for sending data to in-memory cubes
 
-conn = microstrategy.Connection(base_url=base_url, username=username, password=password, project_name=project_name)
-conn.connect()
+mstr_conn = microstrategy.Connection(base_url=mstr_library_api_url, username=mstr_username, password=mstr_password, project_name=mstr_project_name)
+print('1. Connecting to MicroStrategy')
+mstr_conn.connect()
 
 # Loop through all datasets added to the Kaggle Kernel
+print('2. Starting pushing datasets')
 for dirname, dirnames, filenames in os.walk('../input'):
     # loop through all files included in the dataset
+    print('3. Dataset: '+dirname)
     for filename in filenames:
-        print(os.path.join(dirname, filename))
+        print('  Filename: '+os.path.join(dirname, filename))
         dataset_name = filename.replace('.csv', '')
         table_name = dataset_name
-        newDatasetId, newTableId = conn.create_dataset(data_frame=pd.read_csv(os.path.join(dirname, filename)), dataset_name=dataset_name, table_name=table_name)
+        newDatasetId, newTableId = mstr_conn.create_dataset(data_frame=pd.read_csv(os.path.join(dirname, filename)), dataset_name=dataset_name, table_name=table_name)
+    print('3. End of Dataset')
+print('4. All datasets sent, end of script')
